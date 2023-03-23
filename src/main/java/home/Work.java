f1ef5d394121f670ec589b9a3ca123ed20cbf5dc
Jaimi Estrada <jaimiestrada007@gmail.com>
2023-03-23 11:47:11 -0400

    Initial commit

---
+++ src/main/java/home/Work.java
@@ -0,0 +1,117 @@
+package home;
+
+
+
+public class Work {
+
+     /* public static int VowelsCounter(String str) {
+          int count = 0;
+          int i;
+          for (i = 0; i < str.length(); i++) {
+              char charAt = str.charAt(i);
+              if (charAt == 'a' || charAt == 'e' || charAt == 'i' || charAt == 'o' || charAt == 'u' || charAt == 'y') {
+
+
+
+              }
+          }
+          return count;
+      } */
+
+
+   /* question 17: public static String[] swap(String[] stringArray) {
+    if (stringArray== null||stringArray.length < 5 ) {
+
+      return stringArray;
+
+    }
+    String temp = stringArray[0];
+    stringArray[0] = stringArray[stringArray.length - 1];
+    stringArray[stringArray.length-1] = temp;
+
+    return stringArray;
+
+
+
+
+    } */
+
+
+   /* Question 18:  public static String replaceCharacters(String str) {
+        if (str==null)  {
+            return null;
+
+
+        }
+       str = str.replace('f', '7');
+       str = str.replace('s', '$');
+       str = str.replace('1', '!');
+      str =  str.replace('a', '@');
+
+
+
+
+        return str;
+    } */
+
+    /* question 19: public static String replaceWuTangTwoThreeDivisible(String stringInput){
+     if(stringInput==null){
+         return null;
+
+     }
+        stringInput = stringInput.replace("The small brown","The WuTang Wu");
+        stringInput = stringInput.replace("hopped the fence", "Hopped Wu Fence");
+        return stringInput;
+    } */
+
+
+
+          /* question 20  public static String createStringOfFibonnaciUptoMax(int maxnumber) {
+                int fib1 = 0, fib2 = 1 , fib3;
+                StringBuilder result = new StringBuilder();
+                while (fib1 <= maxnumber) {
+                    result.append(fib1).append(" ");
+                    fib3 = fib1 + fib2;
+                    fib1 = fib2;
+                    fib2 = fib3;
+                }
+                return "the answer is " + result;
+            } */
+
+
+
+
+
+
+
+
+
+
+
+            public static void main(String[] args) {
+
+        // String [] someNames = {"eric", "jabba","nelson", "coriander", "bob"};
+
+       // String numberOfVowels = "The number of vowels that we have are";
+
+
+
+
+
+        // String[] stringArray = swap(someNames);
+
+        // System.out.println(Arrays.toString(someNames));
+        //String replaceCharacter = "The Farmer went to the store to get 1 dollar's worth of fertilizer";
+        //System.out.println(replaceCharacters(replaceCharacter));
+       // String theSentence = "The small brown dog hopped the fence";
+       // System.out.println(replaceWuTangTwoThreeDivisible(theSentence));
+
+                //System.out.println(createStringOfFibonnaciUptoMax(25));
+    }
+
+}
+
+
+
+
+
